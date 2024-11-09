d96b82e2652185203379a181aa9b5db5e84fdd46
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-11-09 19:05:31 +0530

    Showing And Hiding Elements

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
 $(function() {
-  // jQuery goes here...
-
-  // Uncomment this line to fade out the red box on page load
-  
-  $(".green-box").fadeOut(1000);
- 
-  $(".green-box").fadeTo(1000,0.5);
-
+  $(".blue-box").hide(1000);
+  $(".blue-box").show(1000);
 });
