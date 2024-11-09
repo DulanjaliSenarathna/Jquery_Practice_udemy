b2f04f64deb7ee998fcf384760321da1f1c3f949
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-11-09 16:54:08 +0530

    A Pitfall With fadeIn/fadeOut vs. fadeTo

--- js/script.js
+++ js/script.js
@@ -3,12 +3,8 @@ $(function() {
 
   // Uncomment this line to fade out the red box on page load
   
-  $(".red-box").fadeTo(1000,0.2);
+  $(".green-box").fadeOut(1000);
  
   $(".green-box").fadeTo(1000,0.5);
 
-  $(".blue-box").fadeTo(1000,0.8);
-
-  $(".blue-box").fadeToggle();
-  $(".blue-box").fadeToggle();
 });
